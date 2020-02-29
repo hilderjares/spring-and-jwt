1115c021fd6783d3af8b1a6fd82058b3ca85e0a6
hilderjares <hider1195@gmail.com>
2020-02-29 16:02:16 -0300

    tests

---
+++ src/test/java/com/hilderjares/spring/SpringAndJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilderjares.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
