fb53e757e7ed6eb87222bbc3793922bf063e6a38
hilderjares <hider1195@gmail.com>
2020-02-29 20:03:18 -0300

    rename entity

--- src/main/java/com/hilderjares/spring/entity/UserDomain.java
+++ src/main/java/com/hilderjares/spring/entity/UserDomain.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class UserDomain {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
