de12f2543d150ced7d492a43baa08e95f1d5e381
hilderjares <hider1195@gmail.com>
2020-02-29 16:03:14 -0300

    spring boot application

---
+++ src/main/java/com/hilderjares/spring/SpringAndJwtApplication.java
@@ -0,0 +1,13 @@
+package com.hilderjares.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndJwtApplication.class, args);
+	}
+
+}
