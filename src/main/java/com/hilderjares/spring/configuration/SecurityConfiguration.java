ca593a5d7a043ecc358498297a3f567181f54292
hilderjares <hider1195@gmail.com>
2020-02-29 16:00:19 -0300

    configuaration application

---
+++ src/main/java/com/hilderjares/spring/configuration/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.hilderjares.spring.configuration;
+
+import static com.hilderjares.spring.security.SecurityConstants.SIGN_UP_URL;
+
+import com.hilderjares.spring.security.JwtAuthorizationFilter;
+import com.hilderjares.spring.service.MyUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+    
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.cors().and().csrf().disable().authorizeRequests().antMatchers(SIGN_UP_URL)
+                .permitAll().anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(
+            new JwtAuthorizationFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class
+        );
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // auth.userDetailsService(this.myUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password(bCryptPasswordEncoder.encode("admin"))
+            .roles("ADMIN");
+    }
+}
