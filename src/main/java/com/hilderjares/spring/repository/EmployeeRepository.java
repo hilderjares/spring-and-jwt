4a06ed8cbecec7d52c44daa496919b6632d3eae3
hilderjares <hider1195@gmail.com>
2020-02-29 16:01:42 -0300

    added repositories

---
+++ src/main/java/com/hilderjares/spring/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.hilderjares.spring.repository;
+
+import com.hilderjares.spring.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/main/java/com/hilderjares/spring/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hilderjares.spring.repository;
+
+import com.hilderjares.spring.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
